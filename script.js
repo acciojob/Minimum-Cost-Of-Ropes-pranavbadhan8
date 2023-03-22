2fb54feb82630edd6ba32c1c87d900e18f6eea35
pranavbadhan8 <120732731+pranavbadhan8@users.noreply.github.com>
2023-03-22 14:19:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,20 @@
- function calculateMinCost() {
-//   var inputData = document.querySelector("#rope-lengths").value;
-//   var inputArr = inputData.split(",");
 
-//   for (var i = 0; i < inputArr.length; i++) {
-//     inputArr[i] = Number(inputArr[i]);
-//   }
-//   var cost = 0;
-// 	inputArr.sort(function (a,b) {return a-b;});
-//   while (inputArr.length > 1) {
-//     var newRope = inputArr[0] + inputArr[1];
-//     cost += newRope;
-//     inputArr.splice(0, 2);
-//     inputArr.push(newRope);
-// 	  inputArr.sort(function (a,b) {return a-b;});
-//   }
-//   document.querySelector("#result").textContent = cost;   
-// }  
-  var inputData = document.querySelector("#rope-lengths").value;
-  var inputArr = inputData.split(",");
+	 function calculateMinCost() {
+      let str = document.querySelector("#rope-lengths").value;
+    let arr = str.split(", ");
 
-  for (var i = 0; i < inputArr.length; i++) {
-    inputArr[i] = Number(inputArr[i]);
-  }
-  var cost = 0;
-	inputArr.sort(function (a,b) {return a-b;});
-  while (inputArr.length > 1) {
-    var newRope = inputArr[0] + inputArr[1];
-    cost += newRope;
-    inputArr.splice(0, 2);
-    inputArr.push(newRope);
-	  inputArr.sort(function (a,b) {return a-b;});
-  }
-  document.querySelector("#result").textContent = cost;   
+    for(let i in arr){
+        arr[i] =  Number(arr[i]);
+    }
+     let mincost=0;
+    while(arr.length!=1){
+        arr.sort( function (a,b){
+            return a-b;
+        });
+        let cost= arr[0] + arr[1];
+        mincost+=cost;
+        arr.splice(0,2);
+        arr.push(cost);
+    }
+    document.querySelector("#result").innerHTML=mincost;
  }
