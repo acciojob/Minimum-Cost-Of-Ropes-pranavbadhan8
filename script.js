91a3c008f2e96b44825091fd76872c2a0a6903f5
pranavbadhan8 <120732731+pranavbadhan8@users.noreply.github.com>
2023-03-22 14:14:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function calculateMinCost() {
+ function calculateMinCost() {
+//   var inputData = document.querySelector("#rope-lengths").value;
+//   var inputArr = inputData.split(",");
+
+//   for (var i = 0; i < inputArr.length; i++) {
+//     inputArr[i] = Number(inputArr[i]);
+//   }
+//   var cost = 0;
+// 	inputArr.sort(function (a,b) {return a-b;});
+//   while (inputArr.length > 1) {
+//     var newRope = inputArr[0] + inputArr[1];
+//     cost += newRope;
+//     inputArr.splice(0, 2);
+//     inputArr.push(newRope);
+// 	  inputArr.sort(function (a,b) {return a-b;});
+//   }
+//   document.querySelector("#result").textContent = cost;   
+// }  
   var inputData = document.querySelector("#rope-lengths").value;
   var inputArr = inputData.split(",");
 
@@ -15,5 +32,4 @@ function calculateMinCost() {
 	  inputArr.sort(function (a,b) {return a-b;});
   }
   document.querySelector("#result").textContent = cost;   
-}  
-  
+ }
